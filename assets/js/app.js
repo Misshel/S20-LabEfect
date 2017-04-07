ab018f6defd11f0ef5e963f10727576d0c5463f9
Misshel <jenmish094@gmail.com>
2017-04-07 18:30:51 -0500

    css fotos v1

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,3 +26,34 @@ window.addEventListener("scroll",function(){
 
   lastScrollTop = currentScroll;
 }, false);
+
+
+/*para fotos*/
+var coders = ['Fiorella Quispe','Lourdes Vilchez','Leydi Maldonado','Yessenia Huamán',
+              'Miriam Mendoza', 'Elizabeth Condori', 'Arantza Burga', 'Grecia Rayme',
+              'Janine Vega', 'Rosario Félix', 'Daguiana Revolledo', 'Jenny Velasquez',
+              'Nadia Cuadros', 'Michelle More', 'Marilu Llamoca', 'Mariel García',
+              'Fiorella Cisneros', 'Geraldine Chauca', 'Yelitza Choque',
+              'Stephanie Hiyagon', 'Rocio Tapia', 'Dana Franco', 'Flor Retamozo',
+              'Nathaly Pacheco', 'Ericka Vidal', 'Katherine Ortega', 'Brilly Majuan',
+              'Flor de Cantuta', 'Leslie Avendaño', 'Cindy Mendoza', 'Annia Flores',
+              'Betsi Loayza', 'Ayda Sulca', 'Milagros Gutierrez', 'Nakarid Jave',
+              'Angie Condor', 'Maricarmen Rojas', 'Ariana Cabana', 'Flor Tello',
+              'Mitchell Rodríguez', 'Naomi Villanueva', 'Mary Castillo', 'Miriam Peralta',
+              'Karin Alejo', 'Liliana Peña', 'Ruth Salvador', 'Marilu Llamoca', 'Wendy Reyes',
+              'Cinthya Quispe','Maria Grecia Cutipa', 'Ana Durand', 'Glisse Jorge', 'Neiza Nuñez',
+              'Sandra Solorzano'];
+for(var i=1;i<54;i++){
+  var foto= document.getElementById("fotos");//section en mi html
+
+  var div=document.createElement("div");
+  var imagen= document.createElement("img");
+  var namecoder= document.createElement("span");
+  imagen.setAttribute("src","assets/image/coders/"+ i +".png");
+  namecoder.innerHTML=coders[i];
+  imagen.setAttribute("class","foto");
+  div.setAttribute("class","imagen");
+  div.appendChild(imagen);
+  div.appendChild(namecoder);
+  foto.appendChild(div);
+}
